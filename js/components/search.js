c0324452077863d200048125d3d6b65bd561465d
dreyhiflden <dreyhiflden@gmail.com>
2019-04-09 16:01:05 +0300

    Add debounce function to improve performance

--- js/components/search.js
+++ js/components/search.js
@@ -1,24 +1,25 @@
 import BaseComponent from './base-component.js';
+import { debounce } from '../services/helpers.js';
 
 class Search extends BaseComponent {
-
   _render() {
     this._element.innerHTML = `
       <p>
         Search:
         <input>
       </p>
-    `;
+      `;
 
     this.addListeners();
   }
 
   addListeners() {
+    const handler = (event) => {
+      this._parent.searchUpdated(event.target.value);
+    };
+
     this._element.querySelector('input')
-      .addEventListener('input', (event) => {
-          this._parent.searchUpdated(event.currentTarget.value);
-        }
-      );
+      .addEventListener('input', debounce(handler));
   }
 }
 
